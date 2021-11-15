d87c22112d3a6d6e1aedde1dcfde5ae8dec51148
verttisaurio1 <1daescalantevertti@gmail.com>
2021-11-15 08:33:50 +0000

    Final changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,30 @@
+"""
+translator Api for french and english
+"""
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+apikey = os.environ['apikey']
+url = os.environ['url']
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(version='2018-05-01',authenticator=authenticator)
+language_translator.set_service_url(url)
+
+def english_to_french(english_text='ERROR'):
+    """this function translates to en"""
+    translation_response = language_translator.translate(\
+    text=english_text, model_id='en-fr')
+    translation=translation_response.get_result()
+    french_text = translation['translations'][0]['translation']
+    return french_text
+
+def french_to_english(french_text='ERROR'):
+    """this function translates to fr"""
+    translation_response = language_translator.translate(\
+    text=french_text, model_id='fr-en')
+    translation=translation_response.get_result()
+    english_text = translation['translations'][0]['translation']
+    return english_text
